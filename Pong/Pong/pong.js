f5294160c1f9e5b451c50afd35f0f211471e3384
DeathswordMJ <64106260+DeathswordMJ@users.noreply.github.com>
2020-05-21 12:21:33 -0700

    LightBox

--- Pong/Pong/pong.js
+++ Pong/Pong/pong.js
@@ -27,7 +27,14 @@ var leftSpeedOfBall = 0;
 var score1 = 0;
 var score2 = 0;
 
+var buzzer = new sound("Buzzer.mp3");
+var bounce = new sound("Bounce.mp3");
 
+
+
+
+
+// start the ball movement
 function startBall() {
 	let direction = 1;
 	topPositionOfBall = startTopPositionOfBall;
@@ -44,11 +51,11 @@ function startBall() {
 	
 }
 
-
+/* start ball moving
 window.addEventListener('load', function(){
 	startBall();
 });
-
+*/
 // moves paddles
 document.addEventListener('keydown', function(e) {
 	//console.log("key down" + e.keyCode);
@@ -100,6 +107,31 @@ document.addEventListener('keyup', function(e) {
 	
 });
 
+// object constructor to play sounds
+// https://www.w3schools.com/graphics/game_sound.asp
+function sound(src){
+	this.sound = document.createElement("audio");
+	this.sound.src = src;
+	this.sound.setAttribute("preload", "auto");
+	this.sound.setAttribute("preload", "auto");
+	this.sound.style.display = "none";
+	document.body.appendChild(this.sound);
+	this.play = function(){
+		this.sound.play();
+		
+	}
+	this.stop = function(){
+		this.sound.pause();
+	
+		
+	
+	
+	}
+
+}
+
+
+
 
 // updates locations of paddles and ball
 window.setInterval(function show() {
@@ -141,14 +173,15 @@ window.setInterval(function show() {
 		
 		// if ball hits left paddle, change direction
 		if (topPositionOfBall > positionOfPaddle1 && topPositionOfBall < positionOfPaddle1 +paddleHeight){
+			bounce.play();
 			leftSpeedOfBall *= -1; 
 		}else{	
+			buzzer.play();
 			startBall();
 			score2 = score2 + 1;
 
 
 	} // else
-	
 	}// if
 	
 	// ball on right edge of gameboard
@@ -156,13 +189,16 @@ window.setInterval(function show() {
 		
 		// if ball hits right paddle, change direction
 		if (topPositionOfBall > positionOfPaddle2 && topPositionOfBall < positionOfPaddle2 + paddleHeight){
+			bounce.play();
 			leftSpeedOfBall *= -1; 
 		}else{	
+		buzzer.play();
 		startBall();
 		score1 = score1 + 1;
 		
 
 	} // else
+	
 	}
 
 	
@@ -172,6 +208,7 @@ window.setInterval(function show() {
 	document.getElementById("paddle2").style.top = positionOfPaddle2 + "px";
 	document.getElementById("ball").style.top = topPositionOfBall + "px";
 	document.getElementById("ball").style.left = leftPositionOfBall + "px";
+	
 	document.getElementById("score1").innerHTML = score1;
 	document.getElementById("score2").innerHTML = score2;
 	
@@ -181,4 +218,96 @@ window.setInterval(function show() {
 }, 1000/60); // show
 
 
+// resume game play
+function resumeGame() {
+	if (!controlPlay){
+	  controlPlay = window.setInterval(show, 1000/60 );
+	}
+} // resumeGame
 
+
+// pause game play
+function pauseGame() {
+  window.setInterval(controlPlay);
+  controlPlay = false;
+} // pauseGame
+
+// start game play
+function startGame() {
+	
+	// reset score,paddles and ball
+	score1 = 0;
+	score2 = 0;
+	positionOfPaddle1 = startPositionOfPaddle1;
+	positionOfPaddle2 = startPositionOfPaddle2;
+	
+	startBall();
+	
+
+	if (!controlPlay){
+	  controlPlay = window.setInterval(show, 1000/60 );
+	}
+
+} // resumeGame
+//stop game play
+function stopGame() {
+  window.setInterval(controlPlay);
+  controlPlay = false;
+  
+  // showLightBox with score
+  let message1 = "Tie Game";
+  let message2 = "Close to continue";
+  
+  if (score2 > score1){
+	  message1 = "Player 2 wins with" + score2 + " points!";
+	  message2 = "Player 1 had" + score1 + " points!";
+} else if (score1 > score2){
+	  message1 = "Player 1 wins with" + score1 + " points!";
+	  message2 = "Player 2 had" + score2 + " points!";
+} // else
+	
+	showLightBox(message1, message2)
+
+
+
+} // stopGame
+
+
+
+
+/****** Light Box Code ******/
+
+function changeVisibility(divID){
+		var element = document.getElementById(divID);
+		
+		
+		//if element exists, toggle it's class
+		// between hidden and unhidden
+		if(element) {
+			element.className = (element.className == 'hidden')? 'unhidden' : 'hidden';
+		} // if
+} // changeVisability
+
+// display message in lightbox
+function showLightBox(message, message2){
+	// set messages
+	document.getElementById("message").innerHTML = message;
+	document.getElementById("message2").innerHTML = message2;
+	// show lightbox
+	changeVisibility("lightbox");
+	changeVisibility("boundaryMessage");
+}
+
+// close lightbox
+function continueGame(){
+	
+  changeVisibility("lightbox");
+  changeVisibility("boundaryMessage");
+  
+  // if the game is over, show controls
+	changeVisibility("controls");
+
+
+}
+
+/****** End Light Box Code ******/
